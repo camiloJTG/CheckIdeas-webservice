f898fe839de6a357afd40aa73fdfe5ac7439872c
camiloJTG <camilojtg.dev@gmail.com>
2020-07-24 18:32:16 -0400

    feat: Create express server and config file

---
+++ src/config/config.js
@@ -0,0 +1,6 @@
+export const config = {
+  environment: {
+    port: process.env.PORT || 3000,
+    dev: process.env.NODE_ENV !== "production",
+  },
+};
---
+++ src/config/index.js
@@ -0,0 +1,12 @@
+import "@babel/polyfill";
+import "dotenv/config";
+
+import app from "./server";
+import { config } from "./config";
+
+const main = async () => {
+  app.listen(config.environment.port);
+  console.log(`Server on port ${config.environment.port}`);
+};
+
+main();
