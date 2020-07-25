b2f321ced09758137cbd39f9d87c01ea54e57ce8
camiloJTG <camilojtg.dev@gmail.com>
2020-07-24 22:05:43 -0400

    feat: add router and middlewares to the server

--- src/config/server.js
+++ src/config/server.js
@@ -1,5 +1,21 @@
-import express from "express";
+import express from 'express';
+import router from '../controllers/routers';
+import {
+  errorHandle,
+  logsError,
+} from '../controllers/middlewares/errorHandler';
 
 const app = express();
+
+// global middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routers
+router(app);
+
+// error middlewares
+app.use(logsError);
+app.use(errorHandle);
 
 export default app;
